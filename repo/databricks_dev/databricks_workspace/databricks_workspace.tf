86863e7818904fd03d50ccb1b2a2e0fae8f19dcc
robert altmiller <robert.altmiller@databricks.com>
2024-03-15 11:28:00 -0500

    changes

--- repo/databricks_dev/databricks_workspace/databricks_workspace.tf
+++ repo/databricks_dev/databricks_workspace/databricks_workspace.tf
@@ -4,8 +4,8 @@ module "uc_catalog" {
   databricks_account_id   = var.databricks_account_id
   aws_account_id          = var.aws_account_id
   resource_prefix         = var.resource_prefix
-  uc_catalog_name         = "${var.resource_prefix}-catalog-${local.workspace_id}"
-  workspace_id            = local.workspace_id
+  uc_catalog_name         = "${var.resource_prefix}-catalog-${var.workspace_id}"
+  workspace_id            = var.workspace_id
   workspace_catalog_admin = var.workspace_catalog_admin
   
 }
